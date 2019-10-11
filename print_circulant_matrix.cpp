d009e0870b4461c616d4b8e1f582799f552d8a7a
Xie Xie <DSAD2015@163.com>
2019-10-11 14:07:21 +0800

    Create print_circulant_matrix.cpp

---
+++ print_circulant_matrix.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  vector<int> V = {1, 2, 3, 4, 5};
+  size_t n = V.size();
+  for (size_t i = 0; i < n; ++i)
+  {
+    for (size_t j = n - i; j < n; ++j)
+      cout << V[j] << ' ';
+    for (size_t j = 0; j < n - i; ++j)
+      cout << V[j] << ' ';
+    cout << endl;
+  }
+  return 0;
+}
