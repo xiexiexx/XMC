54db0b3ae71bb37a9c3852fec654975b4bf8c9ee
Xie Xie <DSAD2015@163.com>
2019-10-13 16:21:02 +0800

    Create multi_dimensional_array.cpp

---
+++ multi_dimensional_array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+size_t mixed_radix(const vector<size_t> B, const vector<size_t> V)
+{
+  size_t value = 0;
+  size_t n = V.size();
+  for (size_t i = 0; i < n - 1; ++i)
+    value = (value + V[i]) * B[i + 1];
+  value += V[n - 1];   // A += V.back();
+  return value;
+}
+
+int main()
+{
+  vector<size_t> B = {10, 10, 6, 10, 6, 10};
+  vector<size_t> V = {1, 0, 1, 2, 1, 8};
+  cout << mixed_radix(B, V) << endl;
+  return 0;
+}
