4df34fdb0df3e9207e7132b601a460163ab5ce56
Xie Xie <DSAD2015@163.com>
2019-11-02 13:13:21 +0800

    Create eliminate_vector_elements.cpp

---
+++ eliminate_vector_elements.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  vector<int> V = {17, 62, 43, 52, 73};
+  size_t r = 0;
+  for (size_t i = 0; i < V.size(); ++i)
+    if (V[i] % 2 == 1)
+      V[r++] = V[i];
+  V.resize(r);
+  for (const auto& x : V)
+    cout << x << ' ';
+  cout << endl;
+  return 0;
+}
