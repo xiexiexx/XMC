8399649e05d6777106426474cecbb31687f13e16
Xie Xie <DSAD2015@163.com>
2019-11-18 23:43:59 +0800

    Create subset_binary_representation.cpp

---
+++ subset_binary_representation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  vector<int> V = {1, 2, 3};
+  size_t n = V.size();
+  size_t m = pow(2, n);
+  vector<bool> B(m + 1, false);
+  for (size_t i = 0; i < m; ++i)
+  {
+    for (size_t k = 0; k < n; ++k)
+      if (B[k])
+        cout << V[k] << ' ';
+    cout << endl;
+    size_t k = 0;
+    while (k < n && B[k])
+      B[k++] = false;
+    B[k] = true;
+  }
+  return 0;
+}
