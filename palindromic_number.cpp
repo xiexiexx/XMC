9f1047b5e03b935781b6e27d2a1ff5d42d2e4eec
Xie Xie <DSAD2015@163.com>
2020-02-03 14:15:39 +0800

    Create palindromic_number.cpp

---
+++ palindromic_number.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+// 适用于其他进制, 这里取10进制.
+const int64_t b = 10;
+
+bool palindromic_number(int64_t n)
+{
+  if (n < 0)
+    return false;
+  if (n < b)
+    return true;
+  int64_t h = n;
+  int64_t r = 0;
+  while (h > b)
+  {
+    int64_t t = h / b;
+    r = r * b + (h - t * b);
+    h = t;
+  }
+  if (r <= numeric_limits<int64_t>::max() / b && r * b == n - h)
+    return true;
+  return false;
+}
+
+int main()
+{
+  cout << "max is " << numeric_limits<int64_t>::max() << endl;
+  int64_t n;
+  cin >> n;
+  cout << (palindromic_number(n) ? "Y" : "N") << endl;
+  return 0;
+}
